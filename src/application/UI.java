e2f0546ffc3352523ce86208696c143fcb608dd6
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2024-07-29 13:59:07 -0300

    pequena melhoria na impressao do tabuleiro

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,26 +1,53 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
-public class UI {//o tabuleiro em si, nessa pagina será impressa como está o tabuleiro
+public class UI {// o tabuleiro em si, nessa pagina será impressa como está o tabuleiro
+	
+	//codigos especiais para printar as cores, retiradas no link abaixo segundo a aula
+	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
 	public static void printBoard(ChessPiece[][] pieces) {
-		for (int i=0; i<pieces.length; i++) {//criado um for para mostrar o tabuleiro na tela
-			System.out.print((8 - i) + " ");//feito a 1 linha para começar a parte de cima do tabuleiro
-			for (int j=0; j<pieces.length; j++) {//criado a 2 parte do tabuleiro, para representar as colunas
+		for (int i = 0; i < pieces.length; i++) {// criado um for para mostrar o tabuleiro na tela
+			System.out.print((8 - i) + " ");// feito a 1 linha para começar a parte de cima do tabuleiro
+			for (int j = 0; j < pieces.length; j++) {// criado a 2 parte do tabuleiro, para representar as colunas
 				printPiece(pieces[i][j]);
 			}
-			System.out.println();//quebra de linha depois da ultima linha
+			System.out.println();// quebra de linha depois da ultima linha
 		}
-		System.out.println("  a b c d e f g h");//impressão das letras
+		System.out.println("  a b c d e f g h");// impressão das letras
 	}
-	
+
 	private static void printPiece(ChessPiece piece) {
-		if (piece == null) {//caso não tenha nenhuma peça naquele local
-			System.out.print("-");//é retornado o lugar com o - representando vazio
-		}
-		else {//caso tenha
-			System.out.print(piece);//retorna qual é a peça ali
+		if (piece == null) {// caso não tenha nenhuma peça naquele local
+			System.out.print("-");// é retornado o lugar com o - representando vazio
+		} else {// caso tenha
+			if (piece.getColor() == Color.WHITE) {//se a cor da peça for branca
+				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+			}
+			else {
+				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+			}
 		}
 		System.out.print(" ");
 	}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -4,32 +4,46 @@ import boardgame.Board;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
-public class ChessMatch {//o coração do projeto, com todas as regras do jogo
-	
+public class ChessMatch {// o coração do projeto, com todas as regras do jogo
+
 	public Board board;
-	
-	public ChessMatch() {//criado um construtor padrão, criando um tabuleiro 8 por 8
+
+	public ChessMatch() {// criado um construtor padrão, criando um tabuleiro 8 por 8
 		board = new Board(8, 8);
 		initialSetup();
 	}
-	
-	public ChessPiece[][] getPieces(){//criado para retornar a matriz das peças de xadrez correspondente a essa partida
+
+	public ChessPiece[][] getPieces() {// criado para retornar a matriz das peças de xadrez correspondente a essa
+										// partida
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-		for (int i=0; i<board.getRows(); i++) {//criado um for para as linhas
-			for (int j=0; j<board.getColumns(); j++) {//e um for interior para as colunas
-				mat[i][j] = (ChessPiece) board.piece(i, j);//vai receber a peça via downcast do ChessPiece, para ser interpretado como peça de xadrez, n apenas uma peça generica
+		for (int i = 0; i < board.getRows(); i++) {// criado um for para as linhas
+			for (int j = 0; j < board.getColumns(); j++) {// e um for interior para as colunas
+				mat[i][j] = (ChessPiece) board.piece(i, j);// vai receber a peça via downcast do ChessPiece, para ser
+															// interpretado como peça de xadrez, n apenas uma peça
+															// generica
 			}
 		}
 		return mat;
 	}
-	
-	private void placeNewPiece(char column, int row, ChessPiece piece) {//metodo irá recer as coordenadas do xadrez
-		board.placePiece(piece, new ChessPosition(column, row).toPosition());//passando as coordenadas para um novo chess position com toPosition
+
+	private void placeNewPiece(char column, int row, ChessPiece piece) {// metodo irá recer as coordenadas do xadrez
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());// passando as coordenadas para um novo
+																				// chess position com toPosition
 	}
-	
-	private void initialSetup() {//metodo responsavel para colocar as peças no local inicial
-		placeNewPiece('b', 6, new Rook(board, Color.WHITE));//instanciado a peça Torre, com o tabuleiro, e a cor Branca, dessa vez com as coordenadas do xadrez atualizada
-		placeNewPiece('e', 8, new King(board, Color.BLACK));//instanciado a peça rei no tabuleiro
-		placeNewPiece('e', 1, new King(board, Color.WHITE));//instanciado a peça rei branca no tabuleiro
+
+	private void initialSetup() {// metodo responsavel para colocar as peças no local inicial
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));//instanciados cada peça no tabuleiro
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 }
