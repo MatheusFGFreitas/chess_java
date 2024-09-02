09999aa918c3a77bc36af4e077dd2fe220cdce4c
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2024-09-02 16:48:03 -0300

    jogada especial roque pequeno e grande

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -100,6 +100,23 @@ public class ChessMatch {// o coração do projeto, com todas as regras do jogo
 			piecesOnTheBoard.remove(capturedPiece);
 			capturedPieces.add(capturedPiece);
 		}
+		//movimento roque pequeno
+		if (p instanceof King && target.getColumn() == source.getColumn() + 2) {
+			Position sourceT = new Position(source.getRow(), source.getColumn() + 3);
+			Position targetT = new Position(source.getRow(), source.getColumn() + 1);
+			ChessPiece rook = (ChessPiece)board.removePiece(sourceT);
+			board.placePiece(rook, targetT);
+			rook.increaseMoveCount();
+		}
+			//movimento roque grande
+			if (p instanceof King && target.getColumn() == source.getColumn() - 2) {
+					Position sourceT = new Position(source.getRow(), source.getColumn() - 4);
+					Position targetT = new Position(source.getRow(), source.getColumn() - 1);
+					ChessPiece rook = (ChessPiece)board.removePiece(sourceT);
+					board.placePiece(rook, targetT);
+					rook.increaseMoveCount();
+			}
+		
 		return capturedPiece;
 	}
 	
@@ -113,6 +130,23 @@ public class ChessMatch {// o coração do projeto, com todas as regras do jogo
 			capturedPieces.remove(capturedPiece);//é removido da variavel de peças capturadas
 			piecesOnTheBoard.add(capturedPiece);//e é adicionada novamente ao tabuleiro
 		}
+		//movimento roque pequeno
+			if (p instanceof King && target.getColumn() == source.getColumn() + 2) {//se o rei andou duas casas
+				Position sourceT = new Position(source.getRow(), source.getColumn() + 3);//pega a torre que está a 3 casas
+				Position targetT = new Position(source.getRow(), source.getColumn() + 1);//e coloca do lado do rei
+				ChessPiece rook = (ChessPiece)board.removePiece(targetT);
+				board.placePiece(rook, sourceT);
+				rook.decreaseMoveCount();
+			}
+		//movimento roque grande
+			if (p instanceof King && target.getColumn() == source.getColumn() - 2) {
+				Position sourceT = new Position(source.getRow(), source.getColumn() - 4);
+				Position targetT = new Position(source.getRow(), source.getColumn() - 1);
+				ChessPiece rook = (ChessPiece)board.removePiece(targetT);
+				board.placePiece(rook, sourceT);
+				rook.decreaseMoveCount();
+			}
+	
 	}
 	
 	private void validateSourcePosition(Position position) {//operação para validar caso tenha peça na posição
@@ -201,7 +235,7 @@ public class ChessMatch {// o coração do projeto, com todas as regras do jogo
 		placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('d', 1, new Queen(board, Color.WHITE));
-		placeNewPiece('e', 1, new King(board, Color.WHITE));
+		placeNewPiece('e', 1, new King(board, Color.WHITE, this));//inserido o this para instanciar essa partida a peça
 		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
@@ -219,7 +253,7 @@ public class ChessMatch {// o coração do projeto, com todas as regras do jogo
 		placeNewPiece('b', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('d', 8, new Queen(board, Color.BLACK));
-		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 8, new King(board, Color.BLACK, this));
 		placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
